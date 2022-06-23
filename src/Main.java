f2cd1033572eaa450a060c394b222ff7643208e0
AanKn <1497786960@qq.com>
2022-06-23 19:29:59 +0800

    pull commit

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("hot-fix");
         System.out.println("master");
         System.out.println("hot-fix2");
+        System.out.println("push test");
     }
 }
